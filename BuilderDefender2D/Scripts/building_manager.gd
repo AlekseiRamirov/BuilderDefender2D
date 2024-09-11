596e707310a41f5732a17d5b4400e94534cd8fce
Alex Ramirez <yaramirez68@gmail.com>
2024-09-10 23:31:13 -0500

    Separate camera, building and input manager

--- BuilderDefender2D/Scripts/building_manager.gd
+++ BuilderDefender2D/Scripts/building_manager.gd
@@ -1,31 +1,20 @@
 class_name BuildingManager extends Node
 
 @export var camera: Camera2D
-@export var mouseSelector: Node2D
+@export var followObjectCam: Node2D
 @export var containerBuilding: Node2D
 var buildingType: Resource
 var buildingTypeList: Resource = load("res://BuilderDefender2D/Resources/BuildingTypesData/ListBuildings.tres") as BuildingTypeList
-var pos: Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	buildingType = buildingTypeList.list[0]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pos = GetMouseWorldPosition2D()
-	mouseSelector.global_position = pos
-
-
-func GetMouseWorldPosition2D() -> Vector2:
-	var mouseWorldPosition: Vector2 = camera.get_local_mouse_position()
-	return mouseWorldPosition
-
-
-func SpawnBuildingTypeScene():
+func SpawnBuildingTypeScene(in_pos: Vector2):
 	var instance_scene: Node2D = buildingType.scene.instantiate()
-	instance_scene.position = pos
+	instance_scene.position = in_pos
 	containerBuilding.add_child(instance_scene)
 
 
--- BuilderDefender2D/Scripts/input_manager.gd
+++ BuilderDefender2D/Scripts/input_manager.gd
@@ -1,20 +1,25 @@
 class_name InputManager extends Node
 
 @export var buildingManager: BuildingManager
+@export var cameraManager: CameraManager
+@export var mouseSelector: Node2D
+
+var mouse_pos: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	mouse_pos = cameraManager.camera.get_local_mouse_position()
+	mouseSelector.global_position = mouse_pos
+
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			buildingManager.SpawnBuildingTypeScene()
+			buildingManager.SpawnBuildingTypeScene(mouse_pos)
 	if event is InputEventKey:
 		if event.keycode == KEY_1 && event.pressed:
 			buildingManager.SelectBuildingTypeToSpawn(1)
