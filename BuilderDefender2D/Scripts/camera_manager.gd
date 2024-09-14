90ef0915f10867ff45959275d983ff36c2d024b9
Alex Ramirez <yaramirez68@gmail.com>
2024-09-14 16:08:00 -0500

    Camera zoom added

--- BuilderDefender2D/Scripts/camera_manager.gd
+++ BuilderDefender2D/Scripts/camera_manager.gd
@@ -1,14 +1,19 @@
 class_name CameraManager extends Node
 
+@export_subgroup("Settings references")
 @export var camera: Camera2D
+@export var phantomCamera: PhantomCamera2D
 @export var followObject: Node2D
-@export var speed_movement: float = 250.0
 
+@export_subgroup("Settings camera")
+@export var speed_movement: float = 250.0
+@export var multiplier_zoom: float = 1.0
+
+var camera_zoom: Vector2 = Vector2.ONE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,3 +23,9 @@ func _process(delta: float) -> void:
 func move_object_to_follow(in_pos: Vector2, in_delta: float) -> void:
 	in_pos.normalized()
 	followObject.global_position = in_pos * speed_movement * in_delta
+
+func zoom_camera(in_zoom: Vector2, in_delta) -> void:
+	camera_zoom = phantomCamera.get_zoom()
+	var calculated_zoom: Vector2 = camera_zoom + (in_zoom * multiplier_zoom * in_delta)
+	var clamped_zoom: Vector2 = clamp(calculated_zoom, Vector2(0.7, 0.7), Vector2(2.0, 2.0))
+	phantomCamera.set_zoom(clamped_zoom)
--- BuilderDefender2D/Scripts/input_manager.gd
+++ BuilderDefender2D/Scripts/input_manager.gd
@@ -1,18 +1,12 @@
 class_name InputManager extends Node
 
-enum ACTIONS {
-	Up,
-	Down,
-	Right,
-	Left
-}
-
 @export var buildingManager: BuildingManager
 @export var cameraManager: CameraManager
 @export var mouseSelector: Node2D
 
 var mouse_pos: Vector2 = Vector2.ZERO
 var move_dir: Vector2 = Vector2.ZERO
+var zoom_dir: Vector2 = Vector2.ONE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,7 +17,9 @@ func _process(delta):
 	mouse_pos = cameraManager.camera.get_local_mouse_position()
 	mouseSelector.global_position = mouse_pos
 
+
 func _physics_process(delta: float) -> void:
+	# Zone to move camera with the invisible object
 	if Input.is_action_pressed("Down"):
 		move_dir.y += 1
 	if Input.is_action_pressed("Up"):
@@ -33,6 +29,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("Left"):
 		move_dir.x -= 1
 	cameraManager.move_object_to_follow(move_dir, delta)
+
+	# Zone to zoom the camera
+	if Input.is_action_just_released("Wheel Up"):
+		print("Wheel up?")
+		cameraManager.zoom_camera(zoom_dir, delta)
+	if Input.is_action_just_released("Wheel Down"):
+		print("Wheel down?")
+		cameraManager.zoom_camera(-zoom_dir, delta)
 
 
 func _input(event):
@@ -46,3 +50,14 @@ func _input(event):
 			buildingManager.SelectBuildingTypeToSpawn(2)
 		if event.keycode == KEY_3 && event.pressed:
 			buildingManager.SelectBuildingTypeToSpawn(3)
+	
+	#if event.is_action_pressed("Wheel Up"):
+		#print("Wheel up?")
+		#zoom_dir.x += 1
+		#zoom_dir.y += 1
+		#cameraManager.zoom_camera(zoom_dir)
+	#if event.is_action_pressed("Wheel Down"):
+		#print("Wheel down?")
+		#zoom_dir.x -= 1
+		#zoom_dir.y -= 1
+		#cameraManager.zoom_camera(zoom_dir)
